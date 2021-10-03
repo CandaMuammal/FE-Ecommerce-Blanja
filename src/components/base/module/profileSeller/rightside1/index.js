260e2e68d61e658c6e0d72b31803f39df3ed3f54
CandaMuammal <scmuammal@gmail.com>
2021-10-04 06:17:08 +0700

    cloudinary user

--- src/components/base/module/profileSeller/rightside1/index.js
+++ src/components/base/module/profileSeller/rightside1/index.js
@@ -43,7 +43,7 @@ const Rightside1 = () => {
         e.preventDefault()
         const formData = new FormData()
         formData.append('username', form.username)
-        formData.append('email', form.email) 
+        formData.append('email', form.email)
         formData.append('phoneNumber', form.phoneNumber)
         formData.append('storeName', form.storeName)
         formData.append('image', form.image)
@@ -51,15 +51,15 @@ const Rightside1 = () => {
 
         // formData.append('image', form.image)
         axios.put(`${process.env.REACT_APP_API_URL}v1/user/${idUser}`, formData)
-        // axios.put('http://localhost:4000/v1/user/60236000', formData)
+            // axios.put('http://localhost:4000/v1/user/60236000', formData)
 
             .then((res) => {
                 toast('success updated profile!')
-                localStorage.setItem('username', username)
-                localStorage.setItem('email', email)
-                localStorage.setItem('phoneNumber', phoneNumber)
-                localStorage.setItem('storeName', storeName)
-                localStorage.setItem('image', image)
+                localStorage.setItem('username', form.username)
+                localStorage.setItem('email', form.email)
+                localStorage.setItem('phoneNumber', form.phoneNumber)
+                localStorage.setItem('storeName', form.storeName)
+                localStorage.setItem('image', form.image)
                 // console.log(form.username)
 
             })
@@ -70,56 +70,56 @@ const Rightside1 = () => {
     }
 
     const handleLogout = () => {
-       localStorage.clear()
+        localStorage.clear()
         window.location.href = "/login"
     }
 
-      return (
+    return (
 
-<div className={style.rightside}>
-                <div className={style.rightbox}>
-                    <div className={style.title}>
-                        <h3>My Profile</h3>
-                        <h4>Manage your profile information</h4>
+        <div className={style.rightside}>
+            <div className={style.rightbox}>
+                <div className={style.title}>
+                    <h3>My Profile</h3>
+                    <h4>Manage your profile information</h4>
+                </div>
+                <div className={style.horizontal}></div>
+                <div className={style.body}>
+                    <div className={style.leftbody}>
+
+                        <div className={style.name}>
+                            <label for="name">Name</label>
+                            <input type="text" name="username" value={form.username} onChange={handleChange} placeholder="Johannes Mikael" />
+                        </div>
+                        <div className={style.email}>
+                            <label for="email">Email</label>
+                            <input type="text" name="email" value={form.email} onChange={handleChange} placeholder="johannes@gmail.com" />
+                        </div>
+                        <div className={style.phone}>
+                            <label for="phone">Phone Number</label>
+                            <input type="text" name="phoneNumber" value={form.phoneNumber} onChange={handleChange} placeholder="08901289012" />
+                        </div>
+                        <div className={style.description}>
+                            <label for="description">Store Description</label>
+                            <input type="text" value={form.storeName} name="storeName" onChange={handleChange} placeholder="" />
+                        </div>
+
+
+
+                        <button className={style.save} onClick={handleSubmit}>Save</button>
+                        <button className={style.save} onClick={handleLogout}>Logout</button>
                     </div>
-                    <div className={style.horizontal}></div>
-                    <div className={style.body}>
-                        <div className={style.leftbody}>
-                            
-                                <div className={style.name}>
-                                    <label for="name">Name</label>
-                                    <input type="text" name="username"  value={form.username} onChange={handleChange} placeholder="Johannes Mikael" />
-                                </div>
-                                <div className={style.email}>
-                                    <label for="email">Email</label>
-                                    <input type="text"  name="email" value={form.email} onChange={handleChange} placeholder="johannes@gmail.com" />
-                                </div>
-                                <div className={style.phone}>
-                                    <label for="phone">Phone Number</label>
-                                    <input type="text"  name="phoneNumber" value={form.phoneNumber} onChange={handleChange} placeholder="08901289012" />
-                                </div>
-                                <div className={style.description}>
-                                    <label for="description">Store Description</label>
-                                    <input type="text" value={form.storeName} name="storeName" onChange={handleChange} placeholder="" />
-                                </div>
-
-
-                            
-                            <button className={style.save} onClick={handleSubmit}>Save</button>
-                            <button className={style.save} onClick={handleLogout}>Logout</button>
-                        </div>
-                        <div className={style.vertical}></div>
-                        <div className={style.rightbody}>
-                            <input type="file" onChange={handleInputFile} />
-                            <img src={form.image} alt=""/>
-                            <button>Select Image</button>
-                        </div>
+                    <div className={style.vertical}></div>
+                    <div className={style.rightbody}>
+                        <input type="file" onChange={handleInputFile} />
+                        <img src={form.image} alt="" />
+                        <button>Select Image</button>
                     </div>
                 </div>
             </div>
+        </div>
 
-      ) 
-    }
+    )
+}
 
 
 export default Rightside1
